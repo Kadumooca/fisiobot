963c0f74cf73bfa46230e6b764fba26b67a65342
Kadumooca <tubaronesss@gmail.com>
2026-04-29 15:47:22 -0300

    Create sessao.js

---
+++ src/utils/sessao.js
@@ -0,0 +1,19 @@
+const sessoes = new Map();
+
+function getSessao(telefone) {
+  if (!sessoes.has(telefone)) {
+    sessoes.set(telefone, { etapa: 'menu' });
+  }
+  return sessoes.get(telefone);
+}
+
+function setSessao(telefone, dados) {
+  const atual = getSessao(telefone);
+  sessoes.set(telefone, { ...atual, ...dados });
+}
+
+function resetarSessao(telefone) {
+  sessoes.set(telefone, { etapa: 'menu' });
+}
+
+module.exports = { getSessao, setSessao, resetarSessao };
