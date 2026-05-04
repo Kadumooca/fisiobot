06f8bad479a7c31180184f137d238a2ffc5ca289
Kadumooca <tubaronesss@gmail.com>
2026-05-04 10:24:14 -0300

    Update formatters.js

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,40 +1,18 @@
+function validarCPF(cpf) {
+  return cpf.replace(/\D/g, '').length === 11;
+}
+
 function limparCPF(cpf) {
   return cpf.replace(/\D/g, '');
 }
 
-function validarCPF(cpf) {
-  const limpo = limparCPF(cpf);
-  return limpo.length === 11;
-}
-
 function formatarCPF(cpf) {
-  const limpo = limparCPF(cpf);
-  return limpo.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+  const c = cpf.replace(/\D/g, '');
+  return c.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
 }
 
 function validarData(data) {
-  const regex = /^\d{2}\/\d{2}\/\d{4}$/;
-  if (!regex.test(data)) return false;
-  const [dia, mes, ano] = data.split('/').map(Number);
-  const d = new Date(ano, mes - 1, dia);
-  return d.getFullYear() === ano && d.getMonth() === mes - 1 && d.getDate() === dia;
+  return /^\d{2}\/\d{2}\/\d{4}$/.test(data);
 }
 
-function dataParaAPI(data) {
-  const [dia, mes, ano] = data.split('/');
-  return `${ano}-${mes}-${dia}`;
-}
-
-function dataParaExibicao(data) {
-  const [ano, mes, dia] = data.split('-');
-  return `${dia}/${mes}/${ano}`;
-}
-
-module.exports = {
-  limparCPF,
-  validarCPF,
-  formatarCPF,
-  validarData,
-  dataParaAPI,
-  dataParaExibicao,
-};
+module.exports = { validarCPF, limparCPF, formatarCPF, validarData };
