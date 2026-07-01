e877ccfa282aafb0a77b9f4a69608db1690beb64
Kadumooca <tubaronesss@gmail.com>
2026-07-01 18:43:30 -0300

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  max: 5,
-  idleTimeoutMillis: 30000,
+  max: 3,
+  idleTimeoutMillis: 10000,
   connectionTimeoutMillis: 5000,
 });
 
