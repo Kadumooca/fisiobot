4fcec1c92f4a25d07bebbc4cbdd5b7b0c9f6fa36
Kadumooca <tubaronesss@gmail.com>
2026-09-02 13:08:13 -0300

    Update formatters.js

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,5 +1,26 @@
 function validarCPF(cpf) {
-  return cpf.replace(/\D/g, '').length === 11;
+  const c = cpf.replace(/\D/g, '');
+  if (c.length !== 11) return false;
+  // Sequências com todos os dígitos iguais (ex: 00000000000, 11111111111)
+  // passam na fórmula abaixo mas nunca são CPFs reais válidos.
+  if (/^(\d)\1{10}$/.test(c)) return false;
+
+  const digitos = c.split('').map(Number);
+  const calcularDigito = (fatorInicial) => {
+    let soma = 0;
+    for (let i = 0; i < fatorInicial - 1; i++) {
+      soma += digitos[i] * (fatorInicial - i);
+    }
+    const resto = soma % 11;
+    return resto < 2 ? 0 : 11 - resto;
+  };
+
+  const digito1 = calcularDigito(10);
+  if (digito1 !== digitos[9]) return false;
+  const digito2 = calcularDigito(11);
+  if (digito2 !== digitos[10]) return false;
+
+  return true;
 }
 
 function limparCPF(cpf) {
